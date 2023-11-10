bca1dc22a47a7d7ae8e14e36ed6e536d7d6dda05
matthieuGravy <matthieugravy@gmail.com>
2023-11-10 16:38:03 +0100

    ajout pug

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const errorHandler = require('errorhandler');
+const express = require("express");
+const errorHandler = require("errorhandler");
 
 const app = express();
 const port = process.env.PORT || 3064;
 console.log(port);
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(errorHandler());
+// Configuration du moteur de modèle Pug
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // Créez un dossier 'views' pour vos fichiers Pug
+
+// Middleware pour gérer les erreurs en développement
+if (process.env.NODE_ENV === "development") {
+  app.use(errorHandler());
 } else {
-    app.use((err, req, res, next) => {
-        const code = err.code || 500;
-        res.status(code).json({
-            code: code,
-            message: code === 500 ? null : err.message
-        });
-    })
+  // Middleware pour gérer les erreurs en production
+  app.use((err, req, res, next) => {
+    const code = err.code || 500;
+    res.status(code).json({
+      code: code,
+      message: code === 500 ? null : err.message,
+    });
+  });
 }
 
+// Middleware pour servir les fichiers statiques depuis le dossier 'public'
+app.use(express.static("public"));
 
+// Route pour la page d'accueil rendue avec Pug
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(port);
+// Démarrer le serveur
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
