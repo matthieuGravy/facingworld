1f374586cd119926c823123ac68ddaba347c5e58
matthieuGravy <matthieugravy@gmail.com>
2023-11-11 15:34:39 +0100

    to be continue

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteHtmlPlugin from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ViteHtmlPlugin({
+      inject: {
+        injectData: {
+          title: 'Client' // Les données que à injecter dans le template Pug
+        }
+      }
+    })
   ],
   resolve: {
     alias: {
